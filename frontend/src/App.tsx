31d887af4fca836644a5b09bf371a0e2e7b17074
Haruki Miyazaki <baskekami19990818@gmail.com>
2024-01-25 21:22:02 -0800

    created the base

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import Button from '@mui/material/Button';
+import Icon from '@mui/material/Icon';
+
+function App() {
+  return (<>
+    <Icon>settings</Icon>
+    <Button variant="contained">Hello world</Button>
+  </>);
+}
+
+export default App;
