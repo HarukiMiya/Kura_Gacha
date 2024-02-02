20f97ea0f5d52749fafc19a34be42d5b7ba9d68a
Haruki Miyazaki <baskekami19990818@gmail.com>
2024-02-02 18:35:29 +0900

    completed the header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,15 +1,17 @@
+import styles from './Header.module.css';
+
 const Header = () => {
   return (
-    <header>
-        <div>
-            <div>
+    <header className={styles.content_box}>
+        <div className={styles.content_children}>
+            <div className={styles.content_item_left_up}>
                 ひこうしき
             </div>
-            <div>
+            <div className={styles.content_item_left_bottom}>
                 非公式
             </div>
         </div>
-        <div>
+        <div className={styles.content_item_right}>
             くら寿司
         </div>
     </header>
