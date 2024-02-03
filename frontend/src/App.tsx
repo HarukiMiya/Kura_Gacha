796a6646a8010223757d451b7956a69190dff997
Haruki Miyazaki <baskekami19990818@gmail.com>
2024-02-03 11:45:59 +0900

    completed the main button and fixed some css for others

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,15 @@
-import Button from '@mui/material/Button';
 import Header from './components/Header';
 import YenGatcha from './components/YenGacha';
 import Footer from './components/Footer';
 import Setting from './components/Setting';
+import MainButton from './components/MainButton';
 
 function App() {
   return (<>
     <Header />
     <Setting />
     <YenGatcha />
-    <Button variant="contained">Hello world</Button>
+    <MainButton />
     <Footer />
   </>);
 }
--- frontend/src/components/Setting.tsx
+++ frontend/src/components/Setting.tsx
@@ -30,46 +30,47 @@ const Setting = () => {
 
   return (
     <>
-      <Icon onClick={handleClickOpen} className={styles.setting_icon} fontSize='large'>
-        settings
-      </Icon>
-      <BootstrapDialog
-        onClose={handleClose}
-        aria-labelledby="customized-dialog-title"
-        open={open}
-      >
-        <DialogTitle sx={{ m: 0, p: 2 }} id="customized-dialog-title">
-          Modal title
-        </DialogTitle>
-        <IconButton
-          aria-label="close"
-          onClick={handleClose}
-          sx={{
-            position: 'absolute',
-            right: 8,
-            top: 8,
-            color: (theme) => theme.palette.grey[500],
-          }}
+        <Icon onClick={handleClickOpen} className={styles.setting_icon} fontSize='large'>
+        {/* <Icon onClick={handleClickOpen} className={classes.setting_icon} fontSize='large'> */}
+            settings
+        </Icon>
+        <BootstrapDialog
+            onClose={handleClose}
+            aria-labelledby="customized-dialog-title"
+            open={open}
         >
-          <CloseIcon />
-        </IconButton>
-        <DialogContent dividers>
-          <Typography gutterBottom>
-            Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-            consectetur ac, vestibulum at eros.
-          </Typography>
-          <Typography gutterBottom>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
-            Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
-          </Typography>
-          <Typography gutterBottom>
-            Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus
-            magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec
-            ullamcorper nulla non metus auctor fringilla.
-          </Typography>
-        </DialogContent>
-      </BootstrapDialog>
+            <DialogTitle sx={{ m: 0, p: 2 }} id="customized-dialog-title">
+                設定
+            </DialogTitle>
+            <IconButton
+                aria-label="close"
+                onClick={handleClose}
+                sx={{
+                    position: 'absolute',
+                    right: 8,
+                    top: 8,
+                    color: (theme) => theme.palette.grey[500],
+                }}
+            >
+                <CloseIcon />
+            </IconButton>
+            <DialogContent dividers>
+                <Typography gutterBottom>
+                    Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+                    dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+                    consectetur ac, vestibulum at eros.
+                </Typography>
+                <Typography gutterBottom>
+                    Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
+                    Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+                </Typography>
+                <Typography gutterBottom>
+                    Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus
+                    magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec
+                    ullamcorper nulla non metus auctor fringilla.
+                </Typography>
+            </DialogContent>
+        </BootstrapDialog>
     </>
   )
 }
