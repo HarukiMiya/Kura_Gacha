e193b84bfdcfbe4f0e1aea4b195b53122e50a368
Haruki Miyazaki <baskekami19990818@gmail.com>
2024-02-04 14:25:25 +0900

    moved files to MainButton folder

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import Header from './components/Header/Header';
 import YenGatcha from './components/YenGacha';
 import Footer from './components/Footer/Footer';
 import Setting from './components/Setting';
-import MainButton from './components/MainButton';
+import MainButton from './components/MainButton/MainButton';
 
 function App() {
   return (<>
