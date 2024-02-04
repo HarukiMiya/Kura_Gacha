449a6dbb02f57c72cf6724a618a067dab9ea17f3
Haruki Miyazaki <baskekami19990818@gmail.com>
2024-02-04 14:23:38 +0900

    moved files into Header folder

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import Header from './components/Header';
+import Header from './components/Header/Header';
 import YenGatcha from './components/YenGacha';
 import Footer from './components/Footer';
 import Setting from './components/Setting';
