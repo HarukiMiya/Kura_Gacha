af882562b0885ef516343e47f5e769861776efc3
Haruki Miyazaki <baskekami19990818@gmail.com>
2024-03-02 01:43:53 +0900

    added tests

---
+++ frontend/__test__/getRandomItems.test.ts
@@ -0,0 +1,213 @@
+import { describe, expect, it } from 'vitest';
+import { Item } from '../src/interfaces/Sushi';
+import { getRandomItems } from '../src/utils/getRandomItems';
+
+interface Ctx {
+    isExactPrice: boolean,
+    isDuplicatable: boolean,
+    isMaxCal: boolean,
+    isRemovedAlco: boolean,
+    isRemovedNigiri: boolean,
+    isRemovedNigiriIkkan: boolean,
+    isRemovedGunkan: boolean,
+    isRemovedSide: boolean,
+    isRemovedDessert: boolean,
+    desiredPrice: number,
+}
+
+const mockGetValidItems = (ctx:Ctx) => {
+    let items: Item[];
+    let impossible: boolean = false;
+    for (let attempt = 0; attempt < 10000; attempt++) {
+        if (impossible) break;
+        const currItems: Item[]| undefined = getRandomItems(
+            ctx.isExactPrice,
+            ctx.isDuplicatable,
+            ctx.isMaxCal,
+            ctx.isRemovedAlco,
+            ctx.isRemovedNigiri,
+            ctx.isRemovedNigiriIkkan,
+            ctx.isRemovedGunkan,
+            ctx.isRemovedSide,
+            ctx.isRemovedDessert,
+            ctx.desiredPrice
+        );
+        console.log("currItems",currItems);
+        if (currItems.length != 0 && currItems[0].item_name == 'impossible') {
+            console.log("currItems", currItems)
+            console.log(currItems === undefined)
+            impossible = true;
+            return [];
+        }
+        if (currItems.length != 0 && currItems[0].item_name != 'impossible' && currItems[0].item_name != 'invalid') {
+            items = currItems;
+            return items;
+        }
+    }
+    return [];
+};
+
+
+
+describe("getRandomItems", () => {
+    // isExactPrice: TRUE
+    for (let i = 0; i < 1000; i++) {
+        it(`isExactPrice:'true' & isDuplicatable:'true' & isMaxCal:'false' - Test 1.1`, () => {
+            const ctx = {
+                desiredPrice: 1000,
+                isExactPrice: true,
+                isDuplicatable: true,
+                isMaxCal: false,
+                isRemovedAlco: true,
+                isRemovedNigiri: false,
+                isRemovedNigiriIkkan: false,
+                isRemovedGunkan: false,
+                isRemovedSide: false,
+                isRemovedDessert: false,
+            };
+            const items = mockGetValidItems(ctx);
+            const totalPrice: number = items.reduce((acc, comb) => acc + comb.item_price, 0);
+            expect(totalPrice).toBe(1000);
+        });
+    }
+    for (let i = 0; i < 1000; i++) {
+        it(`isExactPrice:'true' & isDuplicatable:'false' & isMaxCal:'false' - Test 1.2`, () => {
+            const ctx = {
+                desiredPrice: 1000,
+                isExactPrice: true,
+                isDuplicatable: false,
+                isMaxCal: false,
+                isRemovedAlco: true,
+                isRemovedNigiri: false,
+                isRemovedNigiriIkkan: false,
+                isRemovedGunkan: false,
+                isRemovedSide: false,
+                isRemovedDessert: false,
+            };
+            const items = mockGetValidItems(ctx);
+            const totalPrice: number = items.reduce((acc, comb) => acc + comb.item_price, 0);
+            expect(totalPrice).toBe(1000);
+        });
+    }
+    for (let i = 0; i < 1000; i++) {
+        it(`isExactPrice:'true' & isDuplicatable:'true' & isMaxCal:'true' - Test 1.3`, () => {
+            const ctx = {
+                desiredPrice: 1000,
+                isExactPrice: true,
+                isDuplicatable: true,
+                isMaxCal: true,
+                isRemovedAlco: true,
+                isRemovedNigiri: false,
+                isRemovedNigiriIkkan: false,
+                isRemovedGunkan: false,
+                isRemovedSide: false,
+                isRemovedDessert: false,
+            };
+            const items = mockGetValidItems(ctx);
+            const totalPrice: number = items.reduce((acc, comb) => acc + comb.item_price, 0);
+            expect(totalPrice).toBe(1000);
+        });
+    }
+    for (let i = 0; i < 1000; i++) {
+        it(`isExactPrice:'true' & isDuplicatable:'false' & isMaxCal:'true' - Test 1.4`, () => {
+            const ctx = {
+                desiredPrice: 1000,
+                isExactPrice: true,
+                isDuplicatable: false,
+                isMaxCal: true,
+                isRemovedAlco: true,
+                isRemovedNigiri: false,
+                isRemovedNigiriIkkan: false,
+                isRemovedGunkan: false,
+                isRemovedSide: false,
+                isRemovedDessert: false,
+            };
+            const items = mockGetValidItems(ctx);
+            const totalPrice: number = items.reduce((acc, comb) => acc + comb.item_price, 0);
+            expect(totalPrice).toBe(1000);
+        });
+    }
+
+
+    // isExactPrice: FALSE
+    for (let i = 0; i < 1000; i++) {
+        it(`isExactPrice:'false' & isDuplicatable:'true' & isMaxCal:'false' - Test 2.1`, () => {
+            const ctx = {
+                desiredPrice: 1000,
+                isExactPrice: false,
+                isDuplicatable: true,
+                isMaxCal: false,
+                isRemovedAlco: true,
+                isRemovedNigiri: false,
+                isRemovedNigiriIkkan: false,
+                isRemovedGunkan: false,
+                isRemovedSide: false,
+                isRemovedDessert: false,
+            };
+            const items = mockGetValidItems(ctx);
+            const totalPrice: number = items.reduce((acc, comb) => acc + comb.item_price, 0);
+            expect(totalPrice).toBeGreaterThanOrEqual(950);
+            expect(totalPrice).toBeLessThanOrEqual(1050);
+        });
+    }
+    for (let i = 0; i < 1000; i++) {
+        it(`isExactPrice:'false' & isDuplicatable:'false' & isMaxCal:'false' - Test 2.2`, () => {
+            const ctx = {
+                desiredPrice: 1000,
+                isExactPrice: false,
+                isDuplicatable: false,
+                isMaxCal: false,
+                isRemovedAlco: true,
+                isRemovedNigiri: false,
+                isRemovedNigiriIkkan: false,
+                isRemovedGunkan: false,
+                isRemovedSide: false,
+                isRemovedDessert: false,
+            };
+            const items = mockGetValidItems(ctx);
+            const totalPrice: number = items.reduce((acc, comb) => acc + comb.item_price, 0);
+            expect(totalPrice).toBeGreaterThanOrEqual(950);
+            expect(totalPrice).toBeLessThanOrEqual(1050);
+        });
+    }
+    for (let i = 0; i < 1000; i++) {
+        it(`isExactPrice:'false' & isDuplicatable:'false' & isMaxCal:'true' - Test 2.3`, () => {
+            const ctx = {
+                desiredPrice: 1000,
+                isExactPrice: false,
+                isDuplicatable: false,
+                isMaxCal: true,
+                isRemovedAlco: true,
+                isRemovedNigiri: false,
+                isRemovedNigiriIkkan: false,
+                isRemovedGunkan: false,
+                isRemovedSide: false,
+                isRemovedDessert: false,
+            };
+            const items = mockGetValidItems(ctx);
+            const totalPrice: number = items.reduce((acc, comb) => acc + comb.item_price, 0);
+            expect(totalPrice).toBeGreaterThanOrEqual(900);
+            expect(totalPrice).toBeLessThanOrEqual(1100);
+        });
+    }
+    for (let i = 0; i < 1000; i++) {
+        it(`isExactPrice:'false' & isDuplicatable:'true' & isMaxCal:'true' - Test 2.4`, () => {
+            const ctx = {
+                desiredPrice: 1000,
+                isExactPrice: false,
+                isDuplicatable: true,
+                isMaxCal: true,
+                isRemovedAlco: true,
+                isRemovedNigiri: false,
+                isRemovedNigiriIkkan: false,
+                isRemovedGunkan: false,
+                isRemovedSide: false,
+                isRemovedDessert: false,
+            };
+            const items = mockGetValidItems(ctx);
+            const totalPrice: number = items.reduce((acc, comb) => acc + comb.item_price, 0);
+            expect(totalPrice).toBeGreaterThanOrEqual(900);
+            expect(totalPrice).toBeLessThanOrEqual(1100);
+        });
+    }
+})
