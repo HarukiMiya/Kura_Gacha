8a74f29680ff5b9ae66d963c2b80431897bb7f8d
Haruki Miyazaki <baskekami19990818@gmail.com>
2024-03-08 23:22:13 +0900

    completed handle Errors on local storage

--- frontend/src/hooks/useLocalStorageNumber.ts
+++ frontend/src/hooks/useLocalStorageNumber.ts
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react';
 const useLocalStorageNumber = (key: string, defaultValue: number) => {
   const [state, setState] = useState<number>(() => {
     const localData = localStorage.getItem(key);
-    return localData ? JSON.parse(localData) : defaultValue;
+    return (localData && parseInt(localData)) ? parseInt(localData) : defaultValue;
   });
   useEffect(() => {
     localStorage.setItem(key, JSON.stringify(state));
